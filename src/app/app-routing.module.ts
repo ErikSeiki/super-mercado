ad75c9f1dd426cc853bb1275c743a096314a6b0c
erik nishikawa <erikseiki@gmail.com>
2021-04-15 12:54:22 -0300

    primeira versão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'Home', component: HomeComponent
+  },
+  {
+    path: 'NovoPedido', component: PedidosComponent, pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
